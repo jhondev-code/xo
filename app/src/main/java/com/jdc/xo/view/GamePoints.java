d1547d68a4bab5a78eb38d44fb23f77d17ad1c07
jhondev-code <jhondev.code@gmail.com>
2020-12-06 17:14:45 -0500

    Agregue comentarios en GamePoints
    He agregado los comentarios que describen los atributos y metodos de la clase

--- app/src/main/java/com/jdc/xo/view/GamePoints.java
+++ app/src/main/java/com/jdc/xo/view/GamePoints.java
@@ -5,16 +5,37 @@ import javax.swing.JPanel;
 
 import java.awt.FlowLayout;
 
+/**
+ * Esta clase se usa solamente para visualizar el numero de juegos ganados por
+ * los jugadores.
+ * 
+ * @author jhon fernandez
+ * @version 1.0
+ */
 public class GamePoints extends JPanel {
     
+    /**
+     * Este widget es usado para mostrar el numero de juegos ganados por el jugador "x"
+     */
     private JLabel xGamerPoints;
+
+    /**
+     * Este widget es usado para mostrar el numero de juegos ganados por el jugador "o"
+     */
     private JLabel oGamerPoints;
 
+    /**
+     * Este constructor inicia colocando los widgets de juegos ganados en la pantalla.
+     * Por defecto los juegos ganados empiezan en 0.
+     */
     public GamePoints() {
         super(new FlowLayout());
         this.paintWidgets();
     }
 
+    /**
+     * Este es el metodo encargado de colocar los widgets en la pantalla
+     */
     private void paintWidgets() {
         this.xGamerPoints = new JLabel();
         this.add(this.xGamerPoints);
@@ -26,22 +47,36 @@ public class GamePoints extends JPanel {
         this.add(this.oGamerPoints);
     }
 
+    /**
+     * Usado para obtener la cantidad de juegos ganados por el jugador "X"
+     * @return juegos ganados por "X"
+     */
     public String getxGamePoints() {
         return this.xGamerPoints.getText();
     }
 
+    /**
+     * Usado para modificar la cantidad de juegos ganados por "X"
+     * @param xGamePoints cantidad de juegos ganados por "X"
+     */
     public void setxGamePoints(String xGamePoints) {
         this.xGamerPoints.setText(xGamePoints);;
     }
 
+    /**
+     * Usado para obtener la cantidad de juegos ganados por el jugador "Y"
+     * @return juegos ganados por "Y"
+     */
     public String getoGamerPoints() {
         return this.oGamerPoints.getText();
     }
 
+    /**
+     * Usado para modificar la cantidad de juegos ganados por "Y"
+     * @param oGamerPoints juegos ganados por "Y"
+     */
     public void setoGamerPoints(String oGamerPoints) {
         this.oGamerPoints.setText(oGamerPoints);;
     }
-
-    
 
 }
