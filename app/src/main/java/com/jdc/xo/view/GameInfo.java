4bf197efb220d348968c317b8f7f6f5df3cfd74f
jhondev-code <jhondev.code@gmail.com>
2020-12-06 16:23:39 -0500

    Actualice la clase GameInfo
    Agregue los campos de jugador e icono

--- app/src/main/java/com/jdc/xo/view/GameInfo.java
+++ app/src/main/java/com/jdc/xo/view/GameInfo.java
@@ -1,5 +1,33 @@
 package com.jdc.xo.view;
 
-public class GameInfo {
+import javax.swing.Icon;
+import javax.swing.JPanel;
+
+import java.awt.FlowLayout;
+
+public class GameInfo extends JPanel {
     
+    private String gamer;
+    private Icon icon;
+
+    public GameInfo() {
+        super(new FlowLayout());
+    }
+
+    public String getGamer() {
+        return this.gamer;
+    }
+
+    public void setGamer(String gamer) {
+        this.gamer = gamer;
+    }
+
+    public Icon getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
+
 }
